180c0c4b2f6ec96a064320dbfadd03d9d72f99f9
Lucia Oliva <89274567+loliva12@users.noreply.github.com>
2023-12-15 16:24:36 -0300

    Add files via upload

---
+++ trivia.pl
@@ -0,0 +1,137 @@
+% Preguntas y respuestas
+pregunta(1, 'Cual es la capital de Francia?', 1, '1-Paris 2-Roma 3-Pekin 4-Ciboulette').
+pregunta(2, 'En que anio se fundo la ONU?', 4, '1-1940 2-1900 3-1955 4-1945').
+pregunta(3, 'Quien escribio Don Quijote de la Mancha?', 3, '1-Jorge Luis Borges 2-Grabiel Garcia Marquez 3-Miguel de Cervantes').
+pregunta(4, 'Cual es el planeta mas grande del sistema solar?', 2, '1-Tierra 2-Jupiter 3-Saturno 4-Neptuno').
+pregunta(5, 'En que anio se llevo a cabo la Revolucion Rusa?', 1, '1-1917 2-1850 3-1845 4-1961').
+pregunta(6, 'Quien fue el primer presidente de Estados Unidos?', 1, '1-George Washington 2-Abraham Lincoln 3-Thomas Jefferson 4-John Adams').
+pregunta(7, 'Cual es el rio mas largo del mundo?', 4, '1-Amazonas 2-Nilo 3-Misisipi 4-Yangtse').
+pregunta(8, 'Quien pinto la Mona Lisa?', 1, '1-Leonardo da Vinci 2-Pablo Picasso 3-Van Gogh 4-Claude Monet').
+pregunta(9, 'En que continente se encuentra el desierto del Sahara?', 2, '1-Asia 2-africa 3-America del Norte 4-Europa').
+pregunta(10, 'Quien fue el autor de la teoria de la relatividad?', 1, '1-Albert Einstein 2-Isaac Newton 3-Nikola Tesla 4-Stephen Hawking').
+pregunta(11, 'Cual es el metal mas abundante en la corteza terrestre?', 1, '1-Aluminio 2-Hierro 3-Cobre 4-Oro').
+pregunta(12, 'Quien escribio "Romeo y Julieta"?', 1, '1-William Shakespeare 2-Charles Dickens 3-Jane Austen 4-Homer').
+pregunta(13, 'En que pais se encuentra la Gran Muralla China?', 3, '1-Japon 2-India 3-China 4-Corea del Sur').
+pregunta(14, 'Quien fue el primer ser humano en viajar al espacio exterior?', 1, '1-Yuri Gagarin 2-Neil Armstrong 3-Buzz Aldrin 4-Valentina Tereshkova').
+pregunta(15, 'Cual es el metal liquido a temperatura ambiente?', 4, '1-Plomo 2-Hierro 3-Cobre 4-Mercurio').
+pregunta(16, 'Que elemento quimico tiene el simbolo Hg?', 4, '1-Hierro 2-Oro 3-Plata 4-Mercurio').
+pregunta(17, 'Cual es el oceano mas grande del mundo?', 1, '1-Oceano Pacifico 2-Oceano Atlantico 3-Oceano indico 4-Oceano Antartico').
+pregunta(18, 'Quien pinto "La Noche Estrellada"?', 2, '1-Pablo Picasso 2-Vincent van Gogh 3-Leonardo da Vinci 4-Claude Monet').
+pregunta(19, 'En que ano se firmo la Declaracion de Independencia de Estados Unidos?', 1, '1-1776 2-1789 3-1799 4-1801').
+pregunta(20, 'Cual es el rio mas largo de America del Sur?', 2, '1-Rio Amazonas 2-Rio Parana 3-Rio Orinoco 4-Rio Magdalena').
+pregunta(21, 'Cual es el gas mas abundante en la atmosfera terrestre?', 1, '1-Nitrogeno 2-Oxigeno 3-CO2 4-Argon').
+pregunta(22, 'En que ano llego Cristobal Colon a America?', 1, '1-1492 2-1456 3-1507 4-1513').
+pregunta(23, 'Cual es el pais mas grande del mundo por area terrestre?', 1, '1-Rusia 2-China 3-Canada 4-Estados Unidos').
+pregunta(24, 'Que cientifico formulo la teoria de la evolucion por seleccion natural?', 1, '1-Charles Darwin 2-Isaac Newton 3-Galileo Galilei 4-Albert Einstein').
+pregunta(25, 'Cual es el segundo planeta del sistema solar?', 3, '1-Tierra 2-Marte 3-Venus 4-Jupiter').
+pregunta(26, 'Que famoso fisico desarrollo la teoria de la relatividad?', 1, '1-Albert Einstein 2-Isaac Newton 3-Nikola Tesla 4-Stephen Hawking').
+pregunta(27, 'En que ano ocurrio la Revolucion Francesa?', 1, '1-1789 2-1804 3-1750 4-1823').
+pregunta(28, 'Cual es el simbolo quimico del oro?', 4, '1-At 2-Ag 3-Fe 4-Au').
+pregunta(29, 'Quien fue el presidente de Estados Unidos durante la Guerra Civil Americana?', 1, '1-Abraham Lincoln 2-George Washington 3-Thomas Jefferson 4-John Adams').
+pregunta(30, 'Cual es el oceano mas pequeno del mundo?', 2, '1-Oceano Atlantico 2-Oceano artico 3-Oceano indico 4-Oceano Pacifico').
+pregunta(31, 'Que famoso inventor desarrollo la bombilla electrica?', 1, '1-Thomas Edison 2-Alexander Graham Bell 3-Nikola Tesla 4-George Washington Carver').
+pregunta(32, 'Cual es la capital de Australia?', 2, '1-Sydney 2-Canberra 3-Melbourne 4-Perth').
+pregunta(33, 'En que ano se llevo a cabo la Revolucion Mexicana?', 1, '1-1910 2-1898 3-1925 4-1905').
+pregunta(34, 'Cual es el metal mas pesado conocido?', 1, '1-Osmio 2-Iridio 3-Plutonio 4-Uranio').
+pregunta(35, 'Quien fue el autor de "1984"?', 1, '1-George Orwell 2-Aldous Huxley 3-Ray Bradbury 4-Margaret Atwood').
+pregunta(36, 'Cual es el pais mas poblado del mundo?', 1, '1-China 2-India 3-Estados Unidos 4-Indonesia').
+pregunta(37, 'Quien fue el primer hombre en pisar la Luna?', 2, '1-Neil Armstrong 2-Buzz Aldrin 3-Michael Collins 4-Yuri Gagarin').
+pregunta(38, 'Cual es el simbolo quimico del carbono?', 1, '1-C 2-Ca 3-Co 4-Cu').
+pregunta(39, 'Quien escribio "Cien anos de soledad"?', 2, '1-Julio Cortazar 2-Gabriel Garcia Marquez 3-Mario Vargas Llosa 4-Pablo Neruda').
+pregunta(40, 'En que ano se inauguro la Torre Eiffel en Paris?', 1, '1-1889 2-1899 3-1879 4-1909').
+pregunta(41, 'Quien fue el primer presidente de Mexico?', 2, '1-Benito Juarez 2-Guadalupe Victoria 3-Vicente Guerrero 4-Miguel Hidalgo').
+pregunta(42, 'Cual es el animal terrestre mas grande del mundo?', 1, '1-Elefante africano 2-Jirafa 3-Rinoceronte 4-Hipopotamo').
+pregunta(43, 'En que ano se firmo la Declaracion de Derechos Humanos de las Naciones Unidas?', 1, '1-1948 2-1954 3-1945 4-1950').
+pregunta(44, 'Cual es el metal utilizado comunmente en pilas y baterias?', 2, '1-Hierro 2-Zinc 3-Aluminio 4-Cobre').
+pregunta(45, 'Quien fue el autor de "El principito"?', 1, '1-Antoine de Saint-Exupery 2-J.K. Rowling 3-F. Scott Fitzgerald 4-Ernest Hemingway').
+pregunta(46, 'En que continente se encuentra el rio Nilo?', 3, '1-Asia 2-Europa 3-africa 4-Oceania').
+pregunta(47, 'Cual es el simbolo quimico del sodio?', 1, '1-Na 2-So 3-Si 4-Se').
+pregunta(48, 'Quien fue el famoso rey de Macedonia que conquisto gran parte del mundo conocido?', 1, '1-Alejandro Magno 2-Cesar Augusto 3-Napoleon Bonaparte 4-Julio Cesar').
+pregunta(49, 'Cual es el pais mas pequeno del mundo en terminos de area terrestre?', 1, '1-Ciudad del Vaticano 2-Monaco 3-Nauru 4-Tuvalu').
+pregunta(50, 'En que ano se firmo el Tratado de Versalles que puso fin a la Primera Guerra Mundial?', 1, '1-1919 2-1921 3-1923 4-1917').
+
+% Lista de preguntas disponibles
+preguntas_disponibles([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+                      21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,
+                      39,40,41,42,43,44,45,46,47,48,49,50]).
+
+% Regla para seleccionar una pregunta al azar
+seleccionar_pregunta(Id, Pregunta, Respuesta, Opciones) :-
+    preguntas_disponibles(Preguntas),
+    length(Preguntas, NumPreguntas),
+    random(1, NumPreguntas, Index),
+    nth0(Index, Preguntas, Id), % Seleccionar una pregunta aleatoria de las disponibles
+    pregunta(Id, Pregunta, Respuesta, Opciones).
+
+% Contadores de respuestas correctas e incorrectas
+:- dynamic(respuestas_correctas/1).  % dynamic para poder modificarlos en tiempo de ejecucion
+:- dynamic(respuestas_incorrectas/1).
+
+% Inicializacion de los contadores
+init_contadores :-
+    retractall(respuestas_correctas(_)),
+    retractall(respuestas_incorrectas(_)),
+    asserta(respuestas_correctas(0)),
+    asserta(respuestas_incorrectas(0)).
+
+% Programa principal
+trivia :-
+    init_contadores, % Inicializa los contadores
+    writeln('Bienvenido a la trivia en Prolog!'),
+    writeln('Responde las siguientes preguntas correctamente:'), nl,
+    jugar_trivia(1, 10, []). % Llamamos a jugar_trivia con el contador, el limite de 10 preguntas y una lista de preguntas ya realizadas.
+
+% Regla para terminar el juego: Contador jugar_trivia llega a 0
+jugar_trivia(_, 0, _) :-
+    writeln('Fin del juego!! Gracias por jugar.'), nl,
+    mostrar_resultados.
+
+% Actualiza el contador de preguntas, el array preguntas realizadas y llama a jugar_trivia con la siguiente pregunta
+jugar_trivia(PreguntaActual, PreguntasRestantes, PreguntasRealizadas) :-
+    seleccionar_pregunta(Id, Pregunta, Respuesta, Opciones),
+    \+ member(Id, PreguntasRealizadas), % Asegura que la pregunta no se haya realizado antes
+    write(Pregunta), nl,
+    write('-- '), 
+    write(Opciones), 
+    write(' --'), nl,
+    write('Tu respuesta: '),
+    read_line_to_string(user_input, RespuestaUsuario),
+    verificar_respuesta(Respuesta, RespuestaUsuario),
+    SiguientePregunta is PreguntaActual + 1,
+    NuevasPreguntasRestantes is PreguntasRestantes - 1,
+    jugar_trivia(SiguientePregunta, NuevasPreguntasRestantes, [Id | PreguntasRealizadas]).
+
+% Regla intermedia para checkear si la pregunta ya se realizo, en ese caso se selecciona otra.
+jugar_trivia(PreguntaActual, PreguntasRestantes, PreguntasRealizadas) :-
+    seleccionar_pregunta(Id, _, _, _), % Si la pregunta ya se realizo, se selecciona otra.
+    member(Id, PreguntasRealizadas),
+    jugar_trivia(PreguntaActual, PreguntasRestantes, PreguntasRealizadas).
+
+% Regla para verificar si la respuesta es correcta o incorrecta
+verificar_respuesta(RespuestaCorrecta, RespuestaUsuario) :-
+    string_lower(RespuestaCorrecta, RespuestaCorrectaLower),
+    string_lower(RespuestaUsuario, RespuestaUsuarioLower),
+    RespuestaCorrectaLower == RespuestaUsuarioLower,
+    write('Respuesta correcta!'), nl, nl,
+    retract(respuestas_correctas(Correctas)),
+    NuevasCorrectas is Correctas + 1,
+    asserta(respuestas_correctas(NuevasCorrectas)).
+
+% En caso de que sea incorrecta, se ejecuta esta cláusula de verificar_respuesta
+% y se actualiza el contador de respuestas incorrectas
+verificar_respuesta(_, _) :-
+    write('Respuesta incorrecta.'), nl, nl,
+    retract(respuestas_incorrectas(Incorrectas)),
+    NuevasIncorrectas is Incorrectas + 1,
+    asserta(respuestas_incorrectas(NuevasIncorrectas)).
+
+% Regla para mostrar los resultados aka los contadores
+mostrar_resultados :-
+    respuestas_correctas(Correctas),
+    respuestas_incorrectas(Incorrectas),
+    writeln('-- Resultados --'),
+    format('Respuestas correctas: ~w~n', [Correctas]),
+    format('Respuestas incorrectas: ~w~n', [Incorrectas]).
+
+% Iniciar el juego
+:- trivia.
